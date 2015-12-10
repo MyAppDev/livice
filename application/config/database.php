29e40a90d9a48c38d59d9af12e270d3028ec0e08
Masahiro Matsuoka <phoenix.mshr@gmail.com>
2015-12-10 12:05:28 +0900

    データベース関連の修正

--- application/config/database.php
+++ application/config/database.php
@@ -95,3 +95,26 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => 'sqlite::memory:',
+// 	'username' => '',
+// 	'password' => '',
+// 	'database' => '',
+// 	'dbdriver' => 'pdo',
+// 	'dbprefix' => '',
+// 	'pconnect' => TRUE,
+// 	'db_debug' => FALSE,
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'autoinit' => TRUE,
+// 	'encrypt'  => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
